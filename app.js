873f8d0c22939c8677f584681dd939b6bb5a5764
amsaif52 <am52saif@gmail.com>
2016-12-21 14:48:40 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	favicon = require('serve-favicon');
+
+mongoose.connect("mongodb://localhost:27017/bookworm");
+var db = mongoose.connection;
+
+db.on('error', console.error.bind('console',"connection error"));
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('view engine','pug');
+app.set('views', __dirname+'/views');
+
+app.use('/statics', express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+
+var routes = require('./routes/index');
+app.get('/',routes);
+
+app.use(function(req,res,next){
+	var err = new Error("Not Found");
+	err.status = 400;
+	next(err);
+});
+
+app.use(function(err,req,res,next){
+	res.status(err.status || 500);
+	res.json({error: {message: err.message}});
+	next();
+});
+
+app.listen(port,function(){
+	console.log(`Listening to Port: ${port}`);
+})
+
